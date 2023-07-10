4f862ef92815344d167f411764fe75d904043a9a
aleksacetkovic <cetkovic.sv77.2022@uns.ac.rs>
2023-07-10 22:28:02 +0200

    Worked on abstract Provider.

--- src/dataProvajderi/IsProvider.java
+++ src/dataProvajderi/IsProvider.java
@@ -1,7 +1,8 @@
 package dataProvajderi;
 
-//import java.io.IOException;
-import java.util.ArrayList;
+//import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import helpers.DefaultDict;
 import helpers.Query;
@@ -9,16 +10,28 @@ import helpers.Updater;
 
 public interface IsProvider<T> {
 		
-	public ArrayList<T> get(Query<T> selektor);
+	/**Method that returns a list of entities that satisfy the query.*/
+	public List<T> get(Query<T> selektor);
+	
+	/**Method that returns an iterator to iterate through all the entities.*/
+	public Iterator<T> get();
+	
+	/**Method that calls updater.update() on every entity that satisfies the crieteria given with the query.*/
 	public void put(Query<T> selektor, Updater<T> updater);
+	
+	/**Method that adds a new entity to the collection.*/
 	public void post(T entitet);
+	
+	/**Method that removes all entities that satisfy the query.*/
 	public void delete(Query<T> selektor);
 	
+	/**Method that returns a string id for given entity. Doesn't require checking whether the entity exists in the system, just returns natural id for entity.*/
 	public String getId(T entitet);
+	
+	/**Method that returns an entity with the given id. If the entity with the given id doesn't exist returns a reference to the deleted instance.*/
 	public T getById(String id);
+	
+	/**Method that returns a map id : entity. The map returns a reference to the deleted entity when you try to acces non existent keys.*/
 	public DefaultDict<String, T> getIds();
 	
-	//public void saveData() throws IOException;
-	//public void loadData() throws IOException;
-
 }
